5786a1f5e7c7cc3096c17b2c05d9cca8db660f32
Marcin <marcin.konow@lczyk.xyz>
2024-06-23 20:31:08 +0100

    bits

---
+++ tests/test_get.py
@@ -0,0 +1,31 @@
+import pytest
+from type_conversion_dict import TypeConversionDict
+
+def test_basic() -> None:
+    d = TypeConversionDict(foo='42', bar='blub')
+    value = d['foo']
+    assert value == '42'
+
+    value = d.get('foo')
+    assert value == '42'
+
+    value = d.get('foo', type=int)
+    assert value == 42
+
+    value = d.get('bar', -1, type=int)
+    assert value == -1
+
+def test_missing() -> None:
+    d = TypeConversionDict(foo='42')
+    value = d.get('bar', -1, type=int)
+    assert value == -1
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_missing_2() -> None:
+    d = TypeConversionDict(foo='42')
+    value = d.get('bar', -1, type=int)
+    assert value == -1
+
+    with pytest.raises(KeyError):
+        value = d.get('bar', type=int)
